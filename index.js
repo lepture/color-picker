697c8fed08d1f5d9c7ffaeb0e18eac996a41a3f2
Hsiaoming Yang <me@lepture.com>
2013-08-02 10:53:46 +0800

    it works

--- index.js
+++ index.js
@@ -20,19 +20,56 @@ var colors = [
 ];
 
 function ColorPicker(options) {
-  var el = createPicker(options);
+  var picker = createPicker(options);
+  this.element = picker.picker;
+  this.editor = picker.editor;
 
-  this.events = events(el, this);
+  this.events = events(this.element, this);
   this.events.bind('click .color-chooser-color', 'choose');
   this.events.bind('keyup .color-picker-editor', 'change');
-
-  this.element = el;
 }
 emitter(ColorPicker.prototype);
 
+
+/**
+ * Choose a color.
+ */
 ColorPicker.prototype.choose = function(e) {
-  console.log(e);
-}
+  var color = e;
+  if (e.preventDefault) {
+    e.preventDefault();
+    color = e.target.getAttribute('href');
+  }
+  this.change(color);
+};
+
+
+/**
+ * Change the color value.
+ */
+ColorPicker.prototype.change = function(e) {
+  var color = e;
+  if (e.target) {
+    color = e.target.value;
+  }
+  if (validColor(color)) {
+    this.editor.value = color;
+    this.editor.style.borderColor = color;
+    this.emit('change', color);
+  }
+};
+
+
+/**
+ * Get or set the color value.
+ */
+ColorPicker.prototype.value = function(color) {
+  if (!color) {
+    // getter
+    return this.editor.value;
+  }
+  this.change(color);
+};
 
 
 /**
@@ -47,10 +84,17 @@ function createPicker(options) {
   picker.className = 'color-picker';
   picker.appendChild(chooser);
   picker.appendChild(editor);
-  return picker;
+  return {
+    picker: picker,
+    chooser: chooser,
+    editor: editor
+  };
 };
 
 
+/**
+ * Create the color chooser with the given `colors`.
+ */
 function createChooser(colors) {
   var fields = colors.map(function(color) {
     return '<a class="color-chooser-color" href="' + color + '" style="background-color: ' + color + '!important"></a>';
@@ -61,11 +105,26 @@ function createChooser(colors) {
   return chooser;
 }
 
+
+/**
+ * Create editor input with the given `color`.
+ */
 function createEditor(color) {
-  color = color || '#000000';
+  color = color || '#333333';
   var input = document.createElement('input');
   input.value = color;
   input.className = 'color-picker-editor';
   input.style.borderColor = color;
   return input;
+}
+
+
+/**
+ * Check if the color is valid.
+ */
+function validColor(color) {
+  if (!color) {
+    return false;
+  }
+  return /^#[0-9a-zA-Z]{6}$/.test(color);
 }
